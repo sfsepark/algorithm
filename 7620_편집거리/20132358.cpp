bca6d79d0b1e9a1ed9e4169f4dc500423fd14692
sfsepark <sfsepark@gmail.com>
2020-04-12 23:41:45 +0900

    7620_편집거리_비트마스킹으로 메모리 최적화

--- 7620_편집거리/20132358.cpp
+++ 7620_편집거리/20132358.cpp
@@ -7,12 +7,35 @@
 using namespace std;
 
 unsigned short dp[2][17005];
+char operator_dp[17005][4251];
+
+void init_operator_dp(int i, int j , int op){
+    int j_index = j / 4;
+    int j_unit = j % 4;
+    
+    int shifted_op = op;
+
+    while(j_unit > 0){
+        shifted_op *= 4;
+        j_unit  --;
+    }
+
+    operator_dp[i][j] += shifted_op;
+}
+
+int output_operator_dp(int i, int j){
+
+}
 
 int main(void){
     string a, b;
+    int a_len, b_len;
 
     cin >> a;
     cin >> b;
+
+    a_len = a.size();
+    b_len = b.size();
 
     int prev_ptr = 1;
 
@@ -20,18 +43,25 @@ int main(void){
         dp[0][i] = i;
     }
 
-    for(auto a_it = a.begin() ; a_it != a.end() ; a_it ++){
-
-        char a_char = *a_it;
+    for(int i = 0 ; i < a_len ; i ++ ){
 
         int cur_ptr = (prev_ptr + 1) % 2;
 
-        for(auto b_it = b.begin() ; b_it != b.end() ; b_it ++){
+        for(int j = 0 ; j < b_len ; j ++){
             int min = INT32_MAX;
-            char b_char = *b_it;
-
-            if(a_char != b_char){
-                
+            int tmp ;
+            
+            //case 'd'
+            if(a[i - 1] == b[j]){
+                tmp = dp[prev_ptr][j] + 1;
+                if(tmp < min){
+                    min = tmp;
+                }
+            }
+            //case 'a'
+            if(j > 0){
+                tmp = dp[cur_ptr][i-1] + 1;
+                if()
             }
         }
 
